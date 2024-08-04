8957e79f7f519abfdc23be1e2182074d5d196798
madsheptian <madsheptian@gmail.com>
2024-08-04 15:15:34 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+let FirstName = "Nama:Febrian"
+
+Fistname = "Nama:Febrian"
+
+console.log (FirstName)
+
+FirstName = "Sheptian"
+console.log (FirstName)
+
+
+
+let Age = 30
+
+console.log (Age=30)
+
+let Number = "081323501166"
+
+console.log (Number="081323501166")
+
+const Fat = true
+
+console.log (fat=true)
