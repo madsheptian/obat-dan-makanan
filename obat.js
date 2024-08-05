f28c5ccbbdbff58c90a9c0cc29cf87f0c84d3cb7
madsheptian <madsheptian@gmail.com>
2024-08-05 07:37:55 +0700

    Add files via upload

---
+++ obat.js
@@ -0,0 +1,17 @@
+let firstName="Toko Obat dan Makanan"
+console.log ("Toko Obat dan Makanan")
+
+let open="pukuldelapantigapuluh"
+console.log (open="pukuldelapantigapuluh")
+
+const close=true
+
+console.log (close)
+
+let paragraph="untuk mendapatkan obat tersebut telpon ke nomor 082234587221"
+
+console.log (paragraph)
+
+let age=20240722
+console.log (age)
+
