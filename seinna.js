6e9f12dff6a88c971af6a0e0dd7c0768dae94a7b
madsheptian <madsheptian@gmail.ocm>
2024-08-07 22:13:27 +0700

    seinna.js

---
+++ seinna.js
@@ -0,0 +1,15 @@
+let firstNamen= "Seinna Khoerunisa"
+console.log ("Seinna Khoerunisa") 
+
+
+let parent= "Sheptian&Elvina"
+console.log (parent)
+
+let play="berbie"
+console.log (play)
+
+let born=2026
+console.log (born)
+
+let age=1
+console.log (age)
