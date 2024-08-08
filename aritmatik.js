a1bd89ade2800f993fa34ee20151cbe3a87702da
madsheptian <madsheptian@gmail.ocm>
2024-08-08 22:32:50 +0700

    disini

---
+++ aritmatik.js
@@ -0,0 +1,47 @@
+let x=100 + 50
+console.log (x)
+
+let a=25
+let b=75
+let c=a+b
+console.log (c)
+
+let d=25
+let e=75
+let f=d*e
+console.log (f)
+
+let h=45
+let i=75
+let j=i % h
+console.log (j)
+
+let k=1200
+let l=50
+let m=k/l
+console.log (m)
+
+
+let firstName="Rumus persegi panjang"
+console.log (firstName)
+let lebar=34
+let panjang=75
+let answer=lebar*panjang
+console.log (answer)
+
+let name="Rumus Lingkaran"
+console.log (name)
+let phi=22/7
+let jari1=24
+let jari2=52
+let luas=phi*jari1*jari2
+console.log (luas)
+
+let job="Rumus Segitiga"
+console.log (job)
+let alpha=1/2
+let beta=75
+let tinggi=150
+let jawaban=alpha*beta*tinggi
+console.log (jawaban)
+
