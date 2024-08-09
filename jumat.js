e05426b8ba9817af46216e86d61a5dd98747c100
madsheptian <madsheptian@gmail.ocm>
2024-08-09 21:21:27 +0700

    simpan disini

---
+++ jumat.js
@@ -0,0 +1,11 @@
+//OBJECT
+
+let person= {fistName:"Elvina", lastName:"Cantik", job:"guru"}
+console.log (person.fistName + person.lastName + "seorang" + person.job)
+let god= {
+    name:"Nurlani",
+    age:"50",
+    adress:"Wanayasa Purwakarta",
+    husband:"Zaenal Abidin"
+}
+console.log (god.name + god.age + "alamat" + god.adress + "Nama Suami" + god.husband)
