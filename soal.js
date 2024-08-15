50497871b78342bde49dfef9ff3fdb898c0e8a6f
madsheptian <madsheptian@gmail.ocm>
2024-08-15 15:54:09 +0700

    simpan disini

--- soal.js
+++ soal.js
@@ -1,11 +1,39 @@
-function toUpper(str) {
-    return str
-        .toLowerCase()
-        .split(' ')
-        .map(function(word) {
-            return word[0].toUpperCase() + word.substr(1);
-        })
-        .join(' ');
-     }
-     console.log(toUpper("i love javascript"))
+function Say (ysd){
+    return ysd
+    .toLowerCase()
+    .split (' ')
+    .map(function(cus){
+        return cus[0].toUpperCase() + word.substr(1)
+    })
+    .join(" ")
+}
+//console.log(Say("i love javascript"))
 
+
+function say (abc){
+// console.log (abc)
+    let iniTamp = abc.split(" ")
+    let iniDatanya = iniTamp
+
+    let cobain = iniDatanya[0].replace(iniDatanya[0][0], iniDatanya[0][0].toUpperCase())
+    let cobain2 = iniDatanya[1].replace(iniDatanya[1][0], iniDatanya[1][0].toUpperCase())
+    let cobain3 = iniDatanya[2].replace(iniDatanya[2][0], iniDatanya[2][0].toUpperCase())
+    // console.log(iniDatanya[0][0]);
+    console.log(cobain + " " + cobain2 + " " + cobain3);
+}
+
+/**
+ * Yang ounya index itu string dan array
+ * yang punya length itu string dan array
+ */
+
+
+let firstName = "septian"
+// console.log(firstName.replace(firstName[0], "S"));
+say("febrian sakit amandel")
+say("elvina aku kangen")
+say("harus rajin solat")
+say("jangan lupa baca")
+say("uang 50000 lagi")
+say("bandung kota kembang")
+say("cimahi kota kecil")
---
+++ tugasdiaz.js
@@ -0,0 +1,19 @@
+/**
+    Buat sebuah function.
+    expektasi input sebuah array of string. dan return hasilnya sebuah string
+    example input: ["djalal", "septian", "febrian"]
+    example output: "djalal,seption,febrian" 
+   */
+  
+  /**
+    Buat sebuah function,
+    expektasi input sebuah string, dan return hasilnay sebuah array of string
+    example input: "djalal,septian,febrian"
+    example output: ["djalal", "septian", "febrian"]
+  */
+
+function say (name){
+    let goal = say ["djalal", "septian", "febrian"]
+    return 
+    
+}
