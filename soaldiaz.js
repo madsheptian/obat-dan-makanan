3c703b80d58f5e649f161a9a47aa496ac3836875
mac <mac@macs-MacBook-Air.local>
2024-08-21 17:36:38 +0700

    simpan disini

---
+++ soaldiaz.js
@@ -0,0 +1,42 @@
+/**
+    Buat sebuah function.
+    expektasi input sebuah array of string. dan return hasilnya sebuah string
+    example input: ["djalal", "septian", "febrian"]
+    example output: "djalal,seption,febrian" 
+   */
+  
+  /**
+    Buat sebuah function,
+    expektasi input sebuah string, dan return hasilnay sebuah array of string
+    example input: "djalal,septian,febrian"
+    example output: ["djalal", "septian", "febrian"]
+  */
+
+    function hantu(data) {
+
+        // console.log(data," ==> THIS");
+        let diaz = data.toString()
+        return diaz
+        
+        
+        
+        // let hantu = data ["djalal", "septian", "febrian"]
+        // return hantu
+    }
+    // let hantu = ("djalal, sheptian, febrian")
+    // console.log(hantu);
+
+    console.log(hantu(["djalal", "septian", "febrian"]));
+    
+    // kesalahan saya susah untuk buat code memanggil function
+
+    // nama function (masukan data parameter yang diisi)
+
+
+    
+    function boom(asik) {
+        let raya = asik.split(",")
+        return raya
+    }
+    console.log(boom("djalal,septian,febrian"));
+    
