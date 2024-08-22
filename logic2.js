133b27644757d6e969b73319c1cf3653f6d07dda
mac <mac@macs-MacBook-Air.local>
2024-08-22 17:01:06 +0700

    simpan

---
+++ logic2.js
@@ -0,0 +1,74 @@
+// console.log("spetian");
+// console.log("spetian");
+
+
+// for (let index = 0; index < 2000; index++) {
+//     // const element = array[index];
+//     console.log("ini yang ke " + index + " =>> septian");
+    
+    
+// }
+
+// for (let index = 0; index < 2; index++) {
+//     // const element = array[index];
+//     console.log("Febrian");
+// }
+
+
+/**
+ * LOOP 1
+ * 
+ * let index = 0
+ * index < 2
+ * 0 < 2 // TRUE
+ *      print "febrian"
+ * index++ // ini increment? penambahan 1
+ * index + 1 = 1
+ * index = 1
+ * 
+ * 
+ * LOOP 2
+ * 
+ * let index = 1
+ * 1 < 2 // true
+ *      print "febrian"
+ * index++
+ * 1 + 1 = 2
+ * 
+ * LOOP 3
+ * 
+ * let index = 2
+ * 2 < 2 // flase
+ */
+
+// for (let index = 1; index <= 10; index++) {
+//     //const element = array[index];
+//     console.log(index);
+    
+// }
+
+
+// let name = ["daffa", "sheptian", "febrian"]
+
+
+// // console.log(name []);
+//  for (let index = 0; index < name.leght; index++) {
+//     //const element = array[index];
+//     console.log(index);
+    
+//  }
+
+function angka(data) {
+        let number = data
+        if (number % 2 === 0) {
+            console.log(number + "genap");
+        } else if  (number % 1 === 0) {
+        console.log(number + "ganjil");
+        } else console.log("bukan ganjil genap");
+
+        for (let index = 0; index < angka; index++) {
+            // const element = array[index];   
+        }
+    }
+    console.log(angka(20));
+    
